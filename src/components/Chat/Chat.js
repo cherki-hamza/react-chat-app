d7163dce5ee7341775e6ab26fe45fcbd9757b138
cherki-hamza <cherki0hamza@gmail.com>
2023-03-07 14:18:09 +0100

    update two for chat app

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { auth } from "../../DB/firebase";
+import { ChatEngine } from "react-chat-engine";
+import { useAuth } from '../../context/AuthContext';
 
 const Chat = () => {
+
+    const navigate = useNavigate();
+
+    const { user } = useAuth();
+
+    console.log(user);
+
+    const handleLogout = async () => {
+        await auth.signOut();
+
+        navigate('/');
+    }
+
     return (
-        <div className='container'>
-            <h2 className='alert alert-warning my-5'> Welcome to Chat Dashboard </h2>
+        <div className='chats-page'>
+            <div className='nav-bar'>
+                <div className='logo-tab'>
+                    ch-chat
+                </div>
+
+                <div
+                    onClick={handleLogout}
+                    className='logout-tab'>
+                    <span></span>
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine
+                height="calc(100vh - 66px)"
+                projectID="d8694cd1-7bfb-40d9-8ff2-7731cade35f5"
+                userName="."
+                userSecret="."
+            >
+
+            </ChatEngine>
         </div>
     )
 }
